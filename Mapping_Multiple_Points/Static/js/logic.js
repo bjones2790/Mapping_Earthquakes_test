ad91604784212256ef4e05b1e9835e702e50c1d2
bjones2790 <bjones.2790@gmail.com>
2022-12-11 16:06:15 -0500

    Lines

--- Mapping_Multiple_Points/Static/js/logic.js
+++ Mapping_Multiple_Points/Static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40, -94.5], 4);
 
 // Get data from cities.js
 let cityData = cities;
